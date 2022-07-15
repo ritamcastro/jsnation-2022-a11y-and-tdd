4aa69033e8b968f1cfda6100916653be74a0ff5d
Rita Castro <ritamcastro@gmail.com>
2022-07-15 17:26:11 +0100

    🧪 Make the text-input molecule a configurable and controlable input

--- src/ui/molecules/text-input/text-input.test.js
+++ src/ui/molecules/text-input/text-input.test.js
@@ -3,11 +3,10 @@ import "./text-input"
 
 describe("text-input module", () => {
 
-    it("has the title and the input box", () => {
-        document.body.innerHTML = `<text-input></text-input>`
+    it("has a custom title and is a controlled input", () => {
+        document.body.innerHTML = `<text-input title="Some random title" value="Awesome"></text-input>`
 
-        expect(document.body.querySelector("p")).toHaveTextContent("Hi, welcome")
-
-        expect(document.body.querySelector("input")).toBeInTheDocument()
+        expect(document.body.querySelector("p")).toHaveTextContent("Some random title")
+        expect(document.body.querySelector("input")).toHaveValue("Awesome")
     })
 })
