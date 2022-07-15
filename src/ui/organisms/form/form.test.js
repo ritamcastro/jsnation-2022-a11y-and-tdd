6ba3b39ea8fa9d2dab57a57318631cfbcf135d23
Rita Castro <ritamcastro@gmail.com>
2022-07-15 17:45:53 +0100

    ♻️ Refactor the test code to make it correct!

--- src/ui/organisms/form/form.test.js
+++ src/ui/organisms/form/form.test.js
@@ -4,8 +4,8 @@ describe("form organism", () => {
     it("asks for first name and last name", () => {
         document.body.innerHTML = `<jsnation-form></jsnation-form>`
 
-        expect(document.body.querySelector("p")).toHaveTextContent("First Name")
-        expect(document.body.querySelector("p")).toHaveTextContent("Last Name")
+        expect(document.body.querySelectorAll("p")[0]).toHaveTextContent("First Name")
+        expect(document.body.querySelectorAll("p")[1]).toHaveTextContent("Last Name")
 
         expect(document.body.querySelector("button")).toHaveTextContent("Send it to Lera!")
     })
