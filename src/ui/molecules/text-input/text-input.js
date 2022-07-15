47d3cf05d591c4dfedd2683cf2843eb8b0fe0e35
Rita Castro <ritamcastro@gmail.com>
2022-07-15 17:23:51 +0100

    ✅ Make the text-input molecule test pass

--- src/ui/molecules/text-input/text-input.js
+++ src/ui/molecules/text-input/text-input.js
@@ -1,3 +1,14 @@
-class TextInput extends HTMLElement { }
+class TextInput extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <p>Hi, welcome</p>
+            <input/>
+        `
+    }
+}
 
 window.customElements.define("text-input", TextInput)
