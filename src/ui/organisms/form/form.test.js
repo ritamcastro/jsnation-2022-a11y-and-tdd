86e6c55aa92784da7f7e0606e566cf4c8013a911
Rita Castro <ritamcastro@gmail.com>
2022-07-19 19:33:14 +0100

    🧪 Add a user driven test to populate the form

--- src/ui/organisms/form/form.test.js
+++ src/ui/organisms/form/form.test.js
@@ -17,4 +17,30 @@ describe("form organism", () => {
         const sendItButton = screen.getByRole("button")
         expect(sendItButton).toHaveValue("Send it to Lera!")
     })
+
+    it("populates data and sends it", async () => {
+        const sendItFn = jest.fn()
+        document.body.innerHTML = "<jsnation-form></jsnation-form>"
+        const form = document.body.querySelector("jsnation-form")
+        form.addEventListener("onSubmit", sendItFn)
+
+        const firstNameInput = screen.getByLabelText("Enter your name:")
+        userEvent.click(firstNameInput)
+
+        await userEvent.type(firstNameInput, "Rita")
+        expect(firstNameInput).toHaveValue("Rita")
+
+        userEvent.tab()
+        expect(screen.getByLabelText("Enter your surname:")).toHaveFocus()
+
+        const lastNameInput = screen.getByRole("textbox", { name: "Enter your surname:" })
+        await userEvent.type(lastNameInput, "Castro")
+
+        expect(lastNameInput).toHaveValue("Castro")
+
+        await userEvent.click(screen.getByRole("button"))
+        expect(sendItFn).toHaveBeenCalledWith(
+            new CustomEvent({ detail: { firstName: "Rita", lastName: "Castro" } })
+        )
+    })
 })
