bafdcd259647dab57d2860152c5e515b1a526c6d
Rita Castro <ritamcastro@gmail.com>
2022-07-19 18:55:07 +0100

    🧪 Add semantics to the form

--- src/ui/organisms/form/form.test.js
+++ src/ui/organisms/form/form.test.js
@@ -3,12 +3,18 @@ import userEvent from "@testing-library/user-event"
 import "./form"
 
 describe("form organism", () => {
-    it("asks for first name and last name", () => {
+    it("asks for first name and last name and sends it", () => {
         document.body.innerHTML = "<jsnation-form></jsnation-form>"
+        const form = document.body.querySelector("jsnation-form")
 
-        expect(screen.getByText("Last Name")).toBeInTheDocument()
-        expect(screen.getByText("First Name")).toBeInTheDocument()
+        // On accessing the form via its role, take a look at this:
+        // https://github.com/testing-library/dom-testing-library/issues/937
+        expect(screen.getByRole("form", { name: "JS Nation asks stuff form" })).toBeInTheDocument()
 
-        expect(screen.getByRole("button", { name: "Send it to Lera!" })).toBeInTheDocument()
+        expect(screen.getByLabelText("Enter your surname:")).toBeInTheDocument()
+        expect(screen.getByLabelText("Enter your name:")).toBeInTheDocument()
+
+        const sendItButton = screen.getByRole("button")
+        expect(sendItButton).toHaveValue("Send it to Lera!")
     })
 })
