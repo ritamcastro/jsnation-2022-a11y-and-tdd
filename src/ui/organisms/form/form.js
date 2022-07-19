cc8b865a1e3133d757674d1c812abe7550ee7a99
Rita Castro <ritamcastro@gmail.com>
2022-07-19 19:33:43 +0100

    ✅ Make usage of FormData api to gather the form data

--- src/ui/organisms/form/form.js
+++ src/ui/organisms/form/form.js
@@ -16,6 +16,33 @@ class JsNationForm extends HTMLElement {
             <input type="submit" value="Send it to Lera!" />
         </form>
         `
+
+        // Submitting the data
+        const form = this.querySelector("form")
+        form.addEventListener("submit", (event) => {
+            event.preventDefault()
+
+            // https://developer.mozilla.org/en-US/docs/Web/API/FormData
+            // This piece of code gets all the form values forms into an object 👇
+            // const formValues = Array.from(formData.entries())
+            //   .reduce((acc, [key, value]) => ({...acc, [key]: value}), {})
+            // console.log(formValues)
+            // eg.:
+            // {
+            //   "firstName": "Hey",
+            //   "lastName": "you",
+            // }
+
+            const formData = new FormData(this.querySelector("form"))
+            const personalData = {
+                firstName: formData.get("firstName"),
+                lastName: formData.get("lastName"),
+            }
+
+            this.dispatchEvent(
+                new CustomEvent("onSubmit", { detail: { personalData } }),
+            )
+        })
     }
 }
 
