d15209a69df9365d80e12989a0dcc8db5c51d212
Rita Castro <ritamcastro@gmail.com>
2022-07-29 10:52:04 +0100

    🧪 Add option for type in text-input

--- src/ui/molecules/text-input/text-input.test.js
+++ src/ui/molecules/text-input/text-input.test.js
@@ -3,14 +3,23 @@ import "./text-input"
 
 describe("text-input module", () => {
 
-    it("has a label and is a controlled input", () => {
-        document.body.innerHTML = `
+  it("has a label and is a controlled input", () => {
+    document.body.innerHTML = `
         <text-input labelText="Some random label" name="text" value="Awesome"></text-input>
       `
-        expect(screen.getByLabelText("Some random label")).toBeInTheDocument()
+    expect(screen.getByLabelText("Some random label")).toBeInTheDocument()
 
-        const input = screen.getByRole("textbox", { name: "Some random label" })
-        expect(input).toHaveValue("Awesome")
-        expect(input).toHaveAttribute("name", "text")
-    })
+    const input = screen.getByRole("textbox", { name: "Some random label" })
+    expect(input).toHaveValue("Awesome")
+    expect(input).toHaveAttribute("name", "text")
+  })
+
+  it("can also be an email input", () => {
+    document.body.innerHTML = `
+      <text-input type="email" labelText="An email" name="email" value="lera@gitnation.org"></text-input>
+    `
+
+    const input = screen.getByRole("textbox", { name: "An email" })
+    expect(input).toHaveProperty("type", "email")
+  })
 })
